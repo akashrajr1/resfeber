0778dd511c9345ef34edd98ac267948b011f733e
Diptark Bose <diptark.bose@gmail.com>
2019-04-13 11:07:41 +0530

    Initial Form Completed

--- screens/InitialFormScreen.js
+++ screens/InitialFormScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ImageBackground, Dimensions, ScrollView, Image, Platform } from 'react-native';
+import { View, ImageBackground, Dimensions, ScrollView, Image, Platform, TextInput, StyleSheet } from 'react-native';
 import { Actions } from "react-native-router-flux";
 import { Container, Header, Content, Card, CardItem, Thumbnail, Button, Icon, Left, Body, Text, Right } from 'native-base';
 
@@ -7,48 +7,69 @@ export class InitialFormScreen extends Component {
   render() {
     return (
       <View style={{flex:1}}>
+      <ImageBackground style={{height: 150, width: Dimensions.get('window').width}} source={require('../src/images/rt.jpg')}>
+        <View style={{top: 10, left: 10, right: 0, bottom: 0}}>
+          <Text style={{fontSize: 20, fontWeight: 'bold', color: '#ffffff'}}>Ready for the road trip of a lifetime?</Text>
+          <Text style={{fontSize: 12, fontWeight: 'bold', color: '#ffffff'}}>Let's get you started! </Text>
+        </View>
+      </ImageBackground>
       <Container>
         <Content>
           <Card>
             <CardItem>
-              <Left>
-                <Body>
-                  <Text>Ready for the most memorable road trip ever?</Text>
-                  <Text note>Let's start with some basic info!</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem cardBody>
-              <Image source={{uri: 'Image URL'}} style={{height: 200, width: null, flex: 1}}/>
+                <Text><Icon name="navigate" />  Going to: </Text>
+                <TextInput style = {styles.input}
+                 underlineColorAndroid = "transparent"
+                 placeholder = "eg. New York"
+                 autoCapitalize = "none"/>
             </CardItem>
             <CardItem>
-              <Left>
-                <Button transparent>
-                  <Icon active name="thumbs-up" />
-                  <Text>12 Likes</Text>
-                </Button>
-              </Left>
-              <Body>
-                <Button transparent>
-                  <Icon active name="chatbubbles" />
-                  <Text>4 Comments</Text>
-                </Button>
-              </Body>
-              <Right>
-                <Text>11h ago</Text>
-              </Right>
+                <Text><Icon name="man" />  No. of People: </Text>
+                <TextInput style = {styles.input}
+                 underlineColorAndroid = "transparent"
+                 placeholder = "eg. 4"
+                 autoCapitalize = "none"/>
+            </CardItem>
+            <CardItem>
+                <Text><Icon name="pizza" />  Meal Budget ($): </Text>
+                <TextInput style = {styles.input}
+                 underlineColorAndroid = "transparent"
+                 placeholder = "eg. 25"
+                 autoCapitalize = "none"/>
+            </CardItem>
+            <CardItem>
+                <Text><Icon name="home" />  Hotel Stay Budget ($): </Text>
+                <TextInput style = {styles.input}
+                 underlineColorAndroid = "transparent"
+                 placeholder = "eg. 200"
+                 autoCapitalize = "none"/>
+            </CardItem>
+            <CardItem>
+              <View style={{width:50}}/>
+              <Button success textStyle={{color: '#87838B'}} onPress={ ()=> Actions.navigation()}>
+                <Icon name="navigate" />
+                <Text> Let's hit the road! </Text>
+              </Button>
             </CardItem>
           </Card>
         </Content>
       </Container>
-          <Text> Yo </Text>
-          <Button transparent textStyle={{color: '#87838B'}} onPress={ ()=> Actions.navigation()}>
-            <Icon name="navigate" />
-            <Text> Go </Text>
-          </Button>
       </View>
     )
   }
 };
 
 export default InitialFormScreen;
+
+
+const styles = StyleSheet.create({
+   container: {
+      paddingTop: 23
+   },
+   input: {
+      margin: 5,
+      height: 35,
+      width: 120,
+      borderWidth: 0.5
+   }
+})
